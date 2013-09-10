58a84dd57c88c9606821045bd58e898cc9ca5e34
angleman <angleman@cormea.com>
2013-09-10 14:57:07 -0600

    fix position issue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var checker = require('npm-license')      // AceMetrix/npm-license
-    thispack = require('./package.json')
+    thispack = require('../../package.json')
 ;
 
 checker.init({
@@ -52,7 +52,7 @@ checker.init({
 		Object.keys(mods).forEach(function(key) {
 			var item = mods[key];
 			if (!thispack || !thispack.name || thispack.name != key) {
-//				if (!thispack || !thispack.dependencies || thispack.dependencies[key]) { // only include non-dev packages
+				if (!thispack || !thispack.dependencies || thispack.dependencies[key]) { // only include non-dev packages
 					if (!first) {
 						results = results + ' ';
 					};
@@ -61,7 +61,7 @@ checker.init({
 					license = license.toLowerCase();
 					var color = (colors[license]) ? colors[license] : '';
 					results = results + '[![' + key + '](http://badgr.co/'+license+'/'+ key +'.png'+color+')](' + item.repo + ')';
-//				}
+				}
 			}
 		});
 		if (any) {
